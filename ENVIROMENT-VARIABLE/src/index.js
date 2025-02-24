aca9953dc84da5c7686ff5bf6c85e196c0dac2ab
Sofonias Mengistu <69535143+Sofoniasm@users.noreply.github.com>
2025-02-24 22:37:16 +0300

    ENV variable

--- ENVIROMENT-VARIABLE/src/index.js
+++ ENVIROMENT-VARIABLE/src/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('Hello From Express!');
+    res.send(`Hello From ${process.env.APP_NAME}!`);
+
 });
 
 app.listen(port, () => {
